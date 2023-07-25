e1898145ee443f954330c3a163c422a463ef81e4
Juansu01 <thejuank0@hotmail.com>
2023-07-25 11:26:35 -0500

    Add new function

---
+++ src/voice_channel_actions.py
@@ -0,0 +1,13 @@
+from utils import get_channel_id
+
+
+async def leave_after_being_left_alone(member, before, client):
+    if len(before.channel.members) > 1 or not client.voice_clients:
+        return
+    for voice_client in client.voice_clients:
+        if voice_client.guild.id == before.channel.guild.id:
+            channel_id = get_channel_id(
+                member.guild.channels, "General")
+            channel = client.get_channel(channel_id)
+            await channel.send(f"No {member.mention} dejándome sola :face_with_symbols_over_mouth:")
+            await voice_client.disconnect()
